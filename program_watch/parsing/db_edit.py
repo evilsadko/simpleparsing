a4c5200e015bfda7e4a7f106b274926c05cd5205
sadko <adoo.ssodl@gmail.com>
2020-07-10 17:08:28 +0300

    version 2

--- program_watch/parsing/db_edit.py
+++ program_watch/parsing/db_edit.py
@@ -38,11 +38,6 @@ class DataBase(object):
      def see_all_user(self):
          u ="user"
          return list(self.db[u].find().skip(40).limit(40))
-#         list = []
-#         for i in self.db[u].find():
-#              i["_id"] = str(i["_id"])
-#              list.append(i)
-#         return list(self.db[u].find())#list  
      
          
      def see_count_user(self):
@@ -161,62 +156,10 @@ if __name__ == "__main__":
         classDB.create_collection("Watch")
         #classDB.db["Watch"].create_index([("title", pymongo.TEXT)], name='search_index', default_language='english')
         classDB.db["Watch"].create_index([("Title", "text")])
-        #F = 
         #F = list(classDB.db["Watch"].find({"$text": {"$search": "Rolex"}}).skip().limit())
         #F = list(classDB.db["Watch"].find({"$text": {"$search": "Rolex"}}).skip(10).limit(100))
         #Count = classDB.db["Watch"].find({"$text": {"$search": "Daytona"}}).count() 
         #F = classDB.full_text("Rolex", 0, 0)
-        
-        #.createIndex( { "title": "text" } )
-        #F = classDB.db["Watch"].find().skip(10).limit(20)
-        #db.reviews.createIndex( { comments: "text" } )
-        #print (classDB.count())
-        #G = classDB.see_all_post_v2(10, 1000, None)
-        #print (len(F[0]), F[1], F[0][10])#(len(list(F)))
-        #classDB.see_client()
-#Daytona       
-#class Car(models.Model):
-#        color = models.CharField(...)
-
-
-#class Person(Model):
-#        name = models.CharField(...)
-#        car = models.ForeignKey(...)
-
-#people = Person.objects.filter(...)
-#for person in people:
-#    car = person.car
-
-#from django.db.models import Q
-#3JAAuSG2n 2K7G, 2020Sold Item
-#0 23 567 9 1 345    900000000
-# 1  4   8 10 12 16 17 18 19
-#
-#3JAAuSG2n 2K7G, 2020Sold Item
-#SLZToYldUZ JCuSn Y25V, 2020Sold Item  
-#3USSoldTR3  2JJun 27P, 32M020Sold Item
-#Jun 25, 2020 , 3:16PM
-
-#'$22.01\u2002\u20029 bids'
-# Получать по индексу
-# 3 2 27 2020
-#my_filter_qs = Q()
-#for creator in creator_list:
-#    my_filter_qs = my_filter_qs | Q(creator=creator)
-#my_model.objects.filter(my_filter_qs)
-#https://ru.bmstu.wiki/%D0%9F%D0%BE%D0%BB%D0%BD%D0%BE%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%BE%D0%B2%D1%8B%D0%B9_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA_%D0%B2_MongoDB
-#db.your_collection.find(
-#  {
-#    $text: { $search: "sport hockey" }
-#  }, {
-#    score: { $meta: "textScore" }
-#  }
-#).sort(
-#  {
-#    score: { $meta: "textScore" }
-#  }
-#).limit(2)
-#https://stackoverflow.com/questions/33541290/how-can-i-create-an-index-with-pymongo
 
 
 
